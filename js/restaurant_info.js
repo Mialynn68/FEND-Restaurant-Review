9c86dac36526a9cc00e8aef7607e4e15ee85dba8
Mialynn68 <tanja.staebler@perfectblue.de>
2018-07-05 11:06:47 +0200

    make site responsive

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoibWlhbHlubiIsImEiOiJjamo0cWEwbmwxOGt1M3Bsa2o4eWI2aWJuIn0.nQYrFQaTNppVLR45hL_mZA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
